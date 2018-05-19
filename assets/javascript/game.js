8b92f796c1224b9b0b8f0ba2d7bc6e34e697d4ad
Hooch <nativetongues09@gmail.com>
2018-05-19 01:03:43 -0400

    Psychic-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,37 +4,41 @@
 var possibles = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var psychic = Math.floor(Math.random()*possibles.length);
-console.log(psychic);
-
 
 //Counters
 
 var wins = 0;
-
 var loss = 0;
-
 var remaining = 10;
-
 var guessed = [];
-document.getElementById("list").innerHTML = guessed;
+document.getElementById("guessed").innerHTML = guessed;
 
-function onclick userInput() {
-    guessed.push(userInput);
-    document.getElementById("list").innerHTML = guessed;
+document.onkeyup = function(event){ 
+
+var userInput = event.key;
+{guessed.push(userInput)
 }
 
 //Rules
 
-if userInput === psychic 
-then wins = + 1
-then alert = "You Won!"
+if (userInput === psychic){ 
+{wins++ & reset()}
+{document.getElementById("wins").innerHTML = wins;}
+{alert("You Won!")}
+}
 
-else remaining = -1
+else {remaining--}
+{document.getElementById("remaining").innerHTML = remaining;}
 
-if remaining = 0
-then loss = + 1
-then alert = "You Lost!"
+if (remaining === 0){
+{loss++ & reset()}
+{document.getElementById("loss").innerHTML = loss;}
+{document.getElementById("remaining").innerHTML = remaining;}
+{document.getElementById("guessed").innerHTML = null;}
+alert("You Lost!");
+}
 
-function reset guessed = []
-
-function reset remaining = 10
+function reset()
+{remaining=10;
+guessed=[];
+}

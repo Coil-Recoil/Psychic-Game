70ad0984b142051f97cdaaafe11157dbbbbadd43
Hooch <nativetongues09@gmail.com>
2018-05-18 22:07:58 -0400

    Psychic-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
 
 //Letters
 
-var possibles = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+var possibles = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var psychic = Math.random[possibles];
+var psychic = Math.floor(Math.random()*possibles.length);
+console.log(psychic);
 
 
 //Counters
@@ -15,13 +16,12 @@ var loss = 0;
 var remaining = 10;
 
 var guessed = [];
-document.getElementById("list").innerHTML = guessed
+document.getElementById("list").innerHTML = guessed;
 
-onclick function userInput() {
+function onclick userInput() {
     guessed.push(userInput);
     document.getElementById("list").innerHTML = guessed;
 }
-
 
 //Rules
 
@@ -35,5 +35,6 @@ if remaining = 0
 then loss = + 1
 then alert = "You Lost!"
 
+function reset guessed = []
 
-
+function reset remaining = 10

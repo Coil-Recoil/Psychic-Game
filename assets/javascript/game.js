4fa8b65cbf5240cebf0ce0ddfc994f26a3bc79d1
Hooch <nativetongues09@gmail.com>
2018-05-19 23:54:20 -0400

    Psychic-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,7 @@ var index = Math.floor(Math.random()*possibles.length);
 var psychic=possibles[index];
 console.log(index);
 console.log(psychic);
+
 //Counters
 
 var wins = 0;
@@ -15,41 +16,37 @@ var remaining = 10;
 var guessed = [];
 var userInput;
 
-document.getElementById("print").innerHTML="hello";
-//document.getElementById("guessed").innerHTML(guessed);
-
-// document.onkeyup = function(event){ 
-// userInput = event.key;
-// guessed.push(userInput);
-// console.log(userInput);
-// };
-
+document.onkeyup = function(event){ 
+userInput = event.key;
+guessed.push(userInput);
+document.getElementById("guessed").innerHTML = guessed;
+console.log(userInput);
 
 //Rules
 
+if (userInput === psychic){ 
+wins++;
+reset();
+document.getElementById("wins").innerHTML = wins;
+alert("You Won!");
+}
 
-// if (userInput === psychic){ 
-// wins++;
-// reset();
-// // document.getElementById("wins").innerHTML(wins);
-// alert("You Won!");
-// }
+else {remaining--;
+console.log("remaining: "+ remaining);
+document.getElementById("remaining").innerHTML = remaining;
+}
 
-// else {remaining--;
-//     console.log("remaining: "+ remaining);
-// // document.getElementById("remaining").innerHTML="remaining";
-// }
+if (remaining === 0){
+loss++;
+reset();
+document.getElementById("loss").innerHTML = loss;
+document.getElementById("remaining").innerHTML = remaining;
+document.getElementById("guessed").innerHTML = guessed;
+alert("You Lost!");
+}
+};
 
-// if (remaining === 0){
-// loss++;
-// reset();
-// document.getElementById("loss").innerHTML(loss);
-// document.getElementById("remaining").innerHTML(remaining);
-// document.getElementById("guessed").innerHTML("");
-// alert("You Lost!");
-// }
-
-// function reset()
-// {remaining=10;
-// guessed=[];
-// };
+function reset()
+{remaining = 10;
+guessed=[];
+};
